502ab5e72d73d39de0d4530d4bf47b5366b40d2c
“Mark <markstaff2@gmail.com>
2022-04-21 17:33:59 +0100

    ffbhbxxssddd

--- server/routes/users.js
+++ server/routes/users.js
@@ -9,7 +9,7 @@ router.route('/').get((req, res) => {
 
 router.route('/add').post((req, res) => {
   const username = req.body.username;
-
+//ddd
   const newUser = new User({username});
 
   newUser.save()
