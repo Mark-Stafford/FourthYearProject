b22528c053c573efed1b4383c8746475c6b52792
“Mark <markstaff2@gmail.com>
2022-04-21 23:09:56 +0100

    ffbhbxxssdddssddddddsssddlkxssss

--- server/server.js
+++ server/server.js
@@ -45,9 +45,9 @@ app.use(cors());
 
 const path = require('path');
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("build"));
+  app.use(express.static("public"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,  "public", "index.html"));
   });
 }
 
