180bc1833f9a001e7aa85c055ad6e355b5a7244b
“Mark <markstaff2@gmail.com>
2022-04-21 14:30:22 +0100

    newllllllcLLLKlklllkddkddkkjjkkkk

--- server/server.js
+++ server/server.js
@@ -29,6 +29,12 @@ app.use(morgan('dev'));
 app.use(cookieParser());
 app.use(cors());
 
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("build"));
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+  });
+}
 
 // routes
 app.use(userRoutes);
