c20311722b9b96f39ebd3108755df60aa1dad7dc
“Mark <markstaff2@gmail.com>
2022-04-25 22:02:45 +0100

    heroku publishsedssssdxssddddd

--- server/server.js
+++ server/server.js
@@ -52,9 +52,9 @@ app.use(cors());
 
 const path = require('path');
 if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+  app.use(express.static("client/public"));
   app.get("*", (req, res) => {
-    res.sendFile(path.resolve(__dirname,  "build", "index.html"));
+    res.sendFile(path.resolve(__dirname,  "public", "index.html"));
   });
 }
 
